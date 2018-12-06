a6cad0d2224a3eabb020f02e9a3c58ebb8eaaac6
Janid Ham <janid.ham20@gmail.com>
2018-12-05 21:58:11 -0600

    Se agregan más rutas de ejemplos con express

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const express = require('express')
 
 const app = express()
 
+app.get('*', (req, res) => {
+  res.status(404).send('<h1>No encontramos lo que buscabas</h1>')
+})
+
 app.get('/', (req, res) => {
   res.status(200).send('<h1>Hello world</h1>')
 })
+
+app.get('/user', (req, res) => {
+  res.status(200).json({ message: 'Hello user' })
+})
+
+
 
 app.listen(3000, () => {
   console.log('Server listening at http://localhost:3000')
