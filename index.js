a5db1ca01000d4c9285e4417baa05f556dcc58f9
Janid Ham <janid.ham20@gmail.com>
2018-12-06 19:53:17 -0600

    Se agregan ejemplos de middlewares.

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const userRoutes = require('./user')
 
 const app = express()
 
+const firstMiddleware = (req, res, next) => {
+  console.log('firstMiddleware')
+
+  next()
+}
+
+const authMiddleware = (req, res, next) => {
+  console.log('authMiddleware')
+
+  next(new Error('Esta es una ruta segura, no la puedes ver'))
+}
+
 // app.get('*', (req, res) => {
 //   res.status(404).send('<h1>No encontramos lo que buscabas</h1>')
 // })
@@ -11,7 +23,25 @@ app.get('/', (req, res) => {
   res.status(200).send('<h1>Hello world</h1>')
 })
 
+app.get('/middleware', firstMiddleware, (req, res) => {
+  res.status(200).json({ message: 'soy la ruta del middleware' })
+})
+
+app.get('/segura', authMiddleware, (req, res) => {
+  res.status(200).json({ message: 'soy la ruta segura' })
+})
+
 app.use('/user', userRoutes)
+
+app.use((err, req, res, next) => {
+  if (err) {
+    console.log(err)
+
+    return res.status(500).json({ error: err.message })
+  }
+
+  next()
+})
 
 // app.get('/user', (req, res) => {
 //   res.status(200).json({ message: 'Hello user' })
