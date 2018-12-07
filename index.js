15e402db94f59fc079068c7c793e5155d10f3bc2
Janid Ham <janid.ham20@gmail.com>
2018-12-06 19:48:13 -0600

    Se agrega conexión a la base de mongo y ejemplos de insert y get

--- index.js
+++ index.js
@@ -1,29 +1,53 @@
 const express = require('express')
+const MongoClient = require('mongodb').MongoClient
+
 const userRoutes = require('./user')
 
 const app = express()
 
-// app.get('*', (req, res) => {
-//   res.status(404).send('<h1>No encontramos lo que buscabas</h1>')
-// })
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-  res.status(200).send('<h1>Hello world</h1>')
+
+const MONGO_URL = `mongodb://curso:cursonode1@ds127644.mlab.com:27644/curso-nodejs`
+
+const client = new MongoClient(MONGO_URL, { useNewUrlParser: true })
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
+  if (err) return console.log(err.message)
+
+  console.log("Connected successfully to server")
+
+  const db = client.db()
 })
 
-app.use('/user', userRoutes)
 
-// app.get('/user', (req, res) => {
-//   res.status(200).json({ message: 'Hello user' })
-// })
 
-// app.get('/user/:id([0-9]+)', (req, res) => {
-//   res.status(200).json({ message: 'user regular', id: req.params.id })
-// })
+app.get('/user', (req, res) => {
+  const user = client.db().collection('user')
+  user.find().toArray()
+    .then((users) => {
+      res.status(200).json({ users })
+    })
+    .catch(err => res.status(500).json({ error: err.message }))
+})
 
-// app.get(/user\/:userId([0-9]+)$/, (req, res) => {
-//   res.status(200).json({ message: 'regular 2', id: req.params.userId })
-// })
+app.post('/user', (req, res) => {
+  const { name } = req.body
+  const user = client.db().collection('user')
+  user.insertOne({ name })
+    .then((inserted) => {
+      res.status(200).json({ inserted })
+    })
+    .catch(err => res.status(500).json({ error: err.message }))
+})
+
+// app.use('/user', userRoutes)
+
+app.get('*', (req, res) => {
+  res.status(404).send('<h1>No encontramos lo que buscabas</h1>')
+})
 
 app.listen(3000, () => {
   console.log('Server listening at http://localhost:3000')
